405e543ddf1ef11c0c745d49b0ba8ae954b6b184
Student  SAYNA <nomenaramarolahy@yahoo.com>
2024-08-27 18:45:03 +0200

    ajout de new folder et new file

---
+++ config/app.php
@@ -0,0 +1,3 @@
+<?php
+
+const APP_NAME = "Todo list";
--- index.php
+++ index.php
@@ -1,83 +1,29 @@
-<!DOCTYPE html>
-<!--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
--->
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Top Navigation</title>
-
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="dist/css/adminlte.min.css">
-</head>
-<body class="hold-transition layout-top-nav">
-<div class="wrapper">
-
-  <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
-    <div class="container">
-      <a href="/" class="navbar-brand">
-        <img src="img/SAYNA_Logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        
-      </a>
-
-      <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse order-3" id="navbarCollapse">
-        <!-- Left navbar links -->
+<?php 
+include('config/app.php');
+include('html/header.php');
+?>
 
 
-        <!-- SEARCH FORM -->
-       
-      </div>
-
-      <!-- Right navbar links -->
-      
-    </div>
-  </nav>
-  <!-- /.navbar -->
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0"> TODO LIST </h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-           
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-
-    <!-- Main content -->
-    <div class="content">
-      <div class="container">
         <div class="row">
           <div class="col-12">
       <div class="card" style="position: relative; left: 0px; top: 0px;">
-              <div class="card-header ui-sortable-handle" style="cursor: move;">
-                <h3 class="card-title">
-                  <i class="ion ion-clipboard mr-1"></i>
-                  To Do List
-                </h3>
 
-                
-              </div>
+             
               <!-- /.card-header -->
               <div class="card-body">
+                <!-- formulaire --> 
+                <form action='addItem.php' method='POST'>
+                <div class="input-group input-group-sm">
+                  
+                        <input type="text" class="form-control">
+                            <span class="input-group-append">
+                      <button type="submit" class="btn btn-info btn-flat">Go!</button>
+                            </span>
+                            
+                </div>
+                </form>
+
+                <!-- list --> 
                 <ul class="todo-list " data-widget="todo-list">
                   <li>
                     <!-- drag handle -->
@@ -105,41 +51,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
              
             </div>
             </div>
-        <!-- /.row -->
-      
-        </div>
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
-
-  <!-- Main Footer -->
-  <footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-      Anything you want
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-  </footer>
-</div>
-<!-- ./wrapper -->
-
-<!-- REQUIRED SCRIPTS -->
-
-<!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
-
-</body>
-</html>
+     
+<?php
+include('html/footer.php');
